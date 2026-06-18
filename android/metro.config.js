a7648a5871edcb259001e4e217c21f9608975804
ShubhamRaheja <shubhamraheja1999@gmail.com>
2026-06-18 17:42:24 +0200

    tsconfig issues fixed

---
+++ android/metro.config.js
@@ -0,0 +1,13 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+const config = {
+  resolver: {
+    extraNodeModules: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  watchFolders: [path.resolve(__dirname, 'src')],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
