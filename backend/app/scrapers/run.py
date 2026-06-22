d0bc23de551cc2d91bb328a30656ccc2c9d9fcfe
ShubhamRaheja <shubhamraheja1999@gmail.com>
2026-06-22 11:35:31 +0200

    fixing merge issues

--- backend/app/scrapers/run.py
+++ backend/app/scrapers/run.py
@@ -1,14 +1,7 @@
 import asyncio
 from ..core.database import init_db
-<<<<<<< HEAD
-from ..models.menu import Menu
-from ..models.location import Location
-from .mensa_hungryelk import MensaHungerElkScraper
-from .mensa_maxplanck import MensaMaxPlanckScraper, MensaNordScraper
-=======
 from .mensa_hungryelk import MensaSuedScraper
 from .mensa_maxplanck import MensaNordScraper
->>>>>>> 7a38f914dad2ab2060532bce4763ad9fe97ede84
 
 SCRAPERS = [
     MensaHungerElkScraper(),
