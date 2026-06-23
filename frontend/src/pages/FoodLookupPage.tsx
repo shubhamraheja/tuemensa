1dc17bb2c0109102c6e99ee11a297b8cdcd0deae
JuliaSchneider01 <julia14.schneider@t-online.de>
2026-06-23 15:12:07 +0200

    lint fix

--- frontend/src/pages/FoodLookupPage.tsx
+++ frontend/src/pages/FoodLookupPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {getMapsConfig, getNearbyFood, NearbyFoodPlace} from '@/services/placeService';
 import './FoodLookupPage.css';
